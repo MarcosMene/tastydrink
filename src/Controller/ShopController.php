519e20aa60630807509901a1679bdb5ba29db54a
Marcos Meneghetti <meneghettimarcos@outlook.com>
2024-02-22 12:04:20 +0100

    FRONT-END shop page

---
+++ src/Controller/ShopController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class ShopController extends AbstractController
+{
+    #[Route('/shop', name: 'app_shop')]
+    public function index(): Response
+    {
+        return $this->render('shop/index.html.twig');
+    }
+}
