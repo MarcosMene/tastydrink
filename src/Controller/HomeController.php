0aea77f6577296742c9cf4ca8d2bad1d6665b873
Marcos Meneghetti <meneghettimarcos@outlook.com>
2024-05-16 10:50:19 +0200

    BACK-END Newsletters homepage

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,19 +4,22 @@ namespace App\Controller;
 
 use App\Classe\Mail;
 use App\Entity\BarTime;
+use App\Entity\Newsletter;
 use App\Entity\Product;
 use App\Entity\ShopTime;
+use App\Form\NewsletterType;
 use App\Repository\HeaderRepository;
 use App\Repository\ProductRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(HeaderRepository $headerRepository, ProductRepository $productRepository, EntityManagerInterface $entityManager): Response
+    public function index(HeaderRepository $headerRepository, ProductRepository $productRepository, EntityManagerInterface $entityManager, Request $request): Response
     {
 
         //schedule of the bar
@@ -29,11 +32,26 @@ class HomeController extends AbstractController
             ->getRepository(ShopTime::class)
             ->findAll();
 
-
-
         // show the 3 new products in home page
         $newProducts = $productRepository->findBy([], ['id' => 'DESC'], 3);
 
+
+        //newsletter
+        $newsletter = new Newsletter();
+        $form = $this->createForm(NewsletterType::class, $newsletter);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($newsletter);
+            $entityManager->flush();
+
+            $mail = new Mail();
+            $mail->send($newsletter->getEmail(), 'Tasty Drink User', 'Confirmation newsletter', 'newsletter.html');
+
+            $this->addFlash('success', 'Your have successfully subscribed to our newsletter');
+
+            return $this->redirectToRoute('app_home');
+        }
 
         return $this->render('pages/home.html.twig', [
             'headers' => $headerRepository->findAll(),
@@ -41,6 +59,7 @@ class HomeController extends AbstractController
             'newProducts' => $newProducts,
             'barSchedules' => $barSchedules,
             'shopSchedules' => $shopSchedules,
+            'NewsletterForm' => $form->createView()
         ]);
     }
 }
---
+++ src/Form/NewsletterType.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Newsletter;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class NewsletterType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('email', EmailType::class, [
+                'label' => 'Email',
+                'attr' => [
+                    'placeholder' => 'Your email',
+                    'class' => 'form-control',
+                    'style' => 'my-4',
+                ],
+            ])
+            ->add('submit', SubmitType::class, [
+                'label' => 'Subscribe',
+                'attr' => [
+                    'class' => 'btn btn-primary btn-link mt-5 mx-auto d-flex'
+                ]
+            ]);;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Newsletter::class,
+        ]);
+    }
+}
